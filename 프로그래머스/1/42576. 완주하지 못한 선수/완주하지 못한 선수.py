eff94d7cc288b07ca9602a8545d94f88ca18a530
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2025-03-12 17:15:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.63 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,6 +1,17 @@
+# 해시 사용
 def solution(participant, completion):
-    participant.sort();
-    completion.sort();
-    for a, b in zip(participant, completion):
-        if a != b: return a
-    return participant[-1]
+    dict = {}
+    for i in participant:
+        if i in dict: dict[i] += 1
+        else: dict[i] = 1
+    for i in completion: dict[i] -= 1
+    for i in dict:
+        if dict[i] > 0: return i
+        
+# 정렬 사용
+# def solution(participant, completion):
+#     participant.sort();
+#     completion.sort();
+#     for a, b in zip(participant, completion):
+#         if a != b: return a
+#     return participant[-1]
