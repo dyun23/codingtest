a9005a111ce106e92122c8c978b74e7fb2fb4f8d
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2025-03-13 19:33:53 +0900

    [Easy] Title: 0104-maximum-depth-of-binary-tree, Time: 0 ms (100%), Memory: 19.2 MB (29.88%) -LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,20 +5,28 @@
 #         self.left = left
 #         self.right = right
 
+# postorder 사용
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None: return 0
-        q = deque()
-        depth = 0
-        q.append((root,1))
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
 
-        while q:
-            cur_node, cur_depth = q.popleft()
-            depth = cur_depth
-            if cur_node.left:
-                q.append((cur_node.left, cur_depth + 1))
-            if cur_node.right:
-                q.append((cur_node.right, cur_depth + 1))
+# level order 방식
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if root is None: return 0
+#         q = deque()
+#         depth = 0
+#         q.append((root,1))
+
+#         while q:
+#             cur_node, cur_depth = q.popleft()
+#             depth = cur_depth
+#             if cur_node.left:
+#                 q.append((cur_node.left, cur_depth + 1))
+#             if cur_node.right:
+#                 q.append((cur_node.right, cur_depth + 1))
                 
-        return depth
-        
+#         return depth
