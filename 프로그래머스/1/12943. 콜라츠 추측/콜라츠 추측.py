fda905d79e701377b49debbd4fd4307a90cf01b6
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2025-09-22 10:43:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,7 @@
+def solution(num):
+    count = 0
+    while num != 1:
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        count += 1
+        if count == 500: return -1
+    return count
